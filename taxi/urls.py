74b79b7f7df8950cbf0f8b5aeedb6418bef5881a
Yaroslav <yaroslav.liubarets@icloud.com>
2025-12-13 14:25:00 +0000

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,51 +1,17 @@
+# taxi/urls.py
 from django.urls import path
-
 from .views import (
-    index,
-    CarListView,
-    CarDetailView,
-    CarCreateView,
-    CarUpdateView,
-    CarDeleteView,
-    DriverListView,
-    DriverDetailView,
-    ManufacturerListView,
-    ManufacturerCreateView,
-    ManufacturerUpdateView,
-    ManufacturerDeleteView,
+    # ... other views
+    DriverCreateView,
+    DriverDeleteView,
+    DriverLicenseUpdateView,
+    toggle_assign_to_car,
 )
 
 urlpatterns = [
-    path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
-    path(
-        "manufacturers/create/",
-        ManufacturerCreateView.as_view(),
-        name="manufacturer-create",
-    ),
-    path(
-        "manufacturers/<int:pk>/update/",
-        ManufacturerUpdateView.as_view(),
-        name="manufacturer-update",
-    ),
-    path(
-        "manufacturers/<int:pk>/delete/",
-        ManufacturerDeleteView.as_view(),
-        name="manufacturer-delete",
-    ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    # ... existing patterns
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path("drivers/<int:pk>/update-license/", DriverLicenseUpdateView.as_view(), name="driver-update-license"),
+    path("cars/<int:pk>/toggle-assign/", toggle_assign_to_car, name="toggle-car-assign"),
 ]
-
-app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,89 +1,63 @@
+# taxi/views.py
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Driver, Car, Manufacturer
+from .models import Driver, Car
+from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
 
+# ... Existing Manufacturer views ...
 
-@login_required
-def index(request):
-    """View function for the home page of the site."""
-
-    num_drivers = Driver.objects.count()
-    num_cars = Car.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
-    context = {
-        "num_drivers": num_drivers,
-        "num_cars": num_cars,
-        "num_manufacturers": num_manufacturers,
-        "num_visits": num_visits + 1,
-    }
-
-    return render(request, "taxi/index.html", context=context)
-
-
-class ManufacturerListView(LoginRequiredMixin, generic.ListView):
-    model = Manufacturer
-    context_object_name = "manufacturer_list"
-    template_name = "taxi/manufacturer_list.html"
-    paginate_by = 5
-
-
-class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
-    model = Manufacturer
-    fields = "__all__"
-    success_url = reverse_lazy("taxi:manufacturer-list")
-
-
-class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Manufacturer
-    fields = "__all__"
-    success_url = reverse_lazy("taxi:manufacturer-list")
-
-
-class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Manufacturer
-    success_url = reverse_lazy("taxi:manufacturer-list")
-
-
-class CarListView(LoginRequiredMixin, generic.ListView):
-    model = Car
-    paginate_by = 5
-    queryset = Car.objects.all().select_related("manufacturer")
-
-
-class CarDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Car
-
+# --- Car Views (Updated to use CarForm) ---
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
-
+    template_name = "taxi/car_form.html"
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_form.html"
 
+# ... Car Delete/Detail/List views ...
 
-class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Car
-    success_url = reverse_lazy("taxi:car-list")
+# --- Driver Views ---
 
-
-class DriverListView(LoginRequiredMixin, generic.ListView):
+class DriverCreateView(LoginRequiredMixin, generic.CreateView):
     model = Driver
-    paginate_by = 5
+    form_class = DriverCreationForm
+    success_url = reverse_lazy("taxi:driver-list")
+    template_name = "taxi/driver_form.html"
 
-
-class DriverDetailView(LoginRequiredMixin, generic.DetailView):
+class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+    success_url = reverse_lazy("taxi:driver-list")
+    template_name = "taxi/driver_confirm_delete.html"
+
+class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Driver
+    form_class = DriverLicenseUpdateForm
+    template_name = "taxi/driver_license_form.html"
+    
+    def get_success_url(self):
+        return reverse("taxi:driver-detail", kwargs={"pk": self.object.pk})
+
+# --- Custom Action ---
+
+@login_required
+def toggle_assign_to_car(request, pk):
+    driver = Driver.objects.get(id=request.user.id)
+    car = get_object_or_404(Car, id=pk)
+    
+    if car in driver.cars.all():
+        driver.cars.remove(car)
+    else:
+        driver.cars.add(car)
+        
+    return HttpResponseRedirect(reverse_lazy("taxi:car-detail", args=[pk]))
