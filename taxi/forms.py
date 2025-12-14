89ff7caedfeffb08528078749f489e3a9eec8721
Yaroslav <yaroslav.liubarets@icloud.com>
2025-12-14 08:56:24 +0000

    Solution 1.1

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,11 +1,10 @@
-# taxi/forms.py
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-from .models import Driver, Car
+from .models import Car
 
 def validate_license_number(license_number):
-    """Validator for license number format: 3 uppercase letters + 5 digits"""
     if len(license_number) != 8:
         raise ValidationError("License number must consist of exactly 8 characters.")
     if not license_number[:3].isupper() or not license_number[:3].isalpha():
@@ -16,7 +15,7 @@ def validate_license_number(license_number):
 
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = Driver
+        model = get_user_model()
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "license_number")
 
     def clean_license_number(self):
@@ -24,7 +23,7 @@ class DriverCreationForm(UserCreationForm):
 
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
-        model = Driver
+        model = get_user_model()
         fields = ["license_number"]
 
     def clean_license_number(self):
@@ -32,7 +31,7 @@ class DriverLicenseUpdateForm(forms.ModelForm):
 
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
-        queryset=Driver.objects.all(),
+        queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,4 @@
-# taxi/views.py
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -6,53 +6,34 @@ from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Driver, Car
+from .models import Car
 from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
 
-# ... Existing Manufacturer views ...
-
-# --- Car Views (Updated to use CarForm) ---
-
-class CarCreateView(LoginRequiredMixin, generic.CreateView):
-    model = Car
-    form_class = CarForm
-    success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_form.html"
-
-class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Car
-    form_class = CarForm
-    success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_form.html"
-
-# ... Car Delete/Detail/List views ...
-
-# --- Driver Views ---
+# ... Manufacturer and Car views from previous steps ...
 
 class DriverCreateView(LoginRequiredMixin, generic.CreateView):
-    model = Driver
+    model = get_user_model()
     form_class = DriverCreationForm
     success_url = reverse_lazy("taxi:driver-list")
     template_name = "taxi/driver_form.html"
 
 class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Driver
+    model = get_user_model()
     success_url = reverse_lazy("taxi:driver-list")
     template_name = "taxi/driver_confirm_delete.html"
 
 class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Driver
+    model = get_user_model()
     form_class = DriverLicenseUpdateForm
     template_name = "taxi/driver_license_form.html"
     
     def get_success_url(self):
         return reverse("taxi:driver-detail", kwargs={"pk": self.object.pk})
 
-# --- Custom Action ---
-
 @login_required
 def toggle_assign_to_car(request, pk):
-    driver = Driver.objects.get(id=request.user.id)
+    # Fix: Use request.user directly instead of querying database
+    driver = request.user
     car = get_object_or_404(Car, id=pk)
     
     if car in driver.cars.all():
